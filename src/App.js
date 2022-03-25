e366bc3819856febc5ece1150e82ce8e0123c7f7
Famila-M <familafami99@outlook.com>
2022-03-25 10:39:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+  let formData = new FormData();
+
+  const onfileChange = (e)=>{
+    console.log(e.target.files[0])
+    if(e.target && e.target.files[0]){
+      formData.append('file', e.target.files[0]);
+    }
+  }
+
+const SubmitFileData = () => {
+  axios.post('https://v2.convertapi.com/upload',
+  (formData)
+  )
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <div className='Upload'>
+       <input type="file" name= "file_upload" onChange={onfileChange} accept=".csv" />
+
+       <div className='Refresh'>
+     <button onClick={() => window.location.reload(false)}>Refresh</button>
+
+     <div className='Reset'>
+     <button
+          onClick={() => window.location.replace(false)}>Reset</button>
+     </div>
+     </div>
+     
+     <div className='Submit'>
+       <button onClick={SubmitFileData}>Submit</button>
+     </div>
+     </div> 
     </div>
   );
-}
+  }
 
 export default App;
